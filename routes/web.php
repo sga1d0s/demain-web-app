5efc6b7a4dbc629cce89bc439e77e61ac665d2ad
Sergio Galdos <galdos.sergio@outlook.com>
2025-09-15 14:28:42 +0200

    [main][add] /health route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/health', function(){
+    try{
+        DB::connection()->getPdo();
+        return response()->json(['ok' => true, 'db' => DB::connection()->getDatabaseName()]);
+    } catch (\Throwable $e) {
+        return response()->json(['ok' => false, 'error' => $e->getMessage()], 500);
+    }
 });
