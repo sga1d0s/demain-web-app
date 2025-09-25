f45ef5ea290135bc08cce8723d445247a98819ee
Sergio Galdos <galdos.sergio@outlook.com>
2025-09-26 00:59:45 +0200

    [dev][update] test webhook

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 Route::get('/health', function(){
     try{
         DB::connection()->getPdo();
-        return response()->json(['OK' => true, 'DB' => DB::connection()->getDatabaseName()]);
+        return response()->json(['OK' => true, 'db' => DB::connection()->getDatabaseName()]);
     } catch (\Throwable $e) {
         return response()->json(['OK' => false, 'error' => $e->getMessage()], 500);
     }
