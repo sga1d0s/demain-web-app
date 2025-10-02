b69b770d8e2cfc34336cf20f5f8a2ea340fecd64
Sergio Galdos <galdos.sergio@outlook.com>
2025-10-02 14:05:21 +0200

    [main] UPDATE icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,13 @@
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <!-- Favicons -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#0d6efd">
+
 </head>
 
 <body
